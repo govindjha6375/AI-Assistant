4794b60f07af36fe66a2a2b3c6299ef67bd4d94d
govindjha6375 <98750255+govindjha6375@users.noreply.github.com>
2024-07-04 11:30:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
+from langchain_community.llms import HuggingFaceEndpoint
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 import streamlit as st
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -86,4 +89,53 @@ def get_response_nutrition(image, prompt):
     model = genai.GenerativeModel('gemini-pro-vision')
     response = model.generate_content([image[0], prompt])
     return response.text
+
+repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+task = "text-generation"
+# Define the template outside the function
+template = """
+You are a  travel assistant chatbot your name is TravelMate.AI designed to help users plan their trips and provide travel-related information. Here are some scenarios you should be able to handle:
+
+1. Booking Flights: Assist users with booking flights to their desired destinations. Ask for departure city, destination city, travel dates, and any specific preferences (e.g., direct flights, airline preferences). Check available airlines and book the tickets accordingly.
+
+2. Booking Hotels: Help users find and book accommodations. Inquire about city or region, check-in/check-out dates, number of guests, and accommodation preferences (e.g., budget, amenities). 
+
+3. Booking Rental Cars: Facilitate the booking of rental cars for travel convenience. Gather details such as pickup/drop-off locations, dates, car preferences (e.g., size, type), and any additional requirements.
+
+4. Destination Information: Provide information about popular travel destinations. Offer insights on attractions, local cuisine, cultural highlights, weather conditions, and best times to visit.
+
+5. Travel Tips: Offer practical travel tips and advice. Topics may include packing essentials, visa requirements, currency exchange, local customs, and safety tips.
+
+6. Weather Updates: Give current weather updates for specific destinations or regions. Include temperature forecasts, precipitation chances, and any weather advisories.
+
+7. Local Attractions: Suggest local attractions and points of interest based on the user's destination. Highlight must-see landmarks, museums, parks, and recreational activities.
+
+8. Customer Service: Address customer service inquiries and provide assistance with travel-related issues. Handle queries about bookings, cancellations, refunds, and general support.
+
+Please ensure responses are informative, accurate, and tailored to the user's queries and preferences. Use natural language to engage users and provide a seamless experience throughout their travel planning journey.
+
+Chat history:
+{chat_history}
+
+User question:
+{user_question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+def get_response_travel(user_query, chat_history):
+    # Initialize the Hugging Face Endpoint
+    llm = HuggingFaceEndpoint(
+        huggingfacehub_api_token="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",
+        repo_id=repo_id,
+        task=task
+    )
+
+    chain = prompt | llm | StrOutputParser()
+
+    response = chain.invoke({
+        "chat_history": chat_history,
+        "user_question": user_query,
+    })
+
+    return response
 
